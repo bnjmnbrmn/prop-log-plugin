c8c923dadd076afab536aedd87017a4d5c806de0
Benjamin Berman <bnjmnbrmn@gmail.com>
2014-07-05 17:55:23 -0500

    Removed dockable--this will be handled by the KCMjEdit plugin

--- src/edu/uiowa/cs/baberman/proplogjedit/PropLogPlugin.java
+++ src/edu/uiowa/cs/baberman/proplogjedit/PropLogPlugin.java
@@ -1,6 +1,8 @@
-
 package edu.uiowa.cs.baberman.proplogjedit;
 
+import edu.uiowa.cs.baberman.kcm.KCMS;
+import edu.uiowa.cs.baberman.kcm.ThirtyKey;
+import javax.swing.JPanel;
 import org.gjt.sp.jedit.EBPlugin;
 import org.gjt.sp.jedit.Macros;
 import org.gjt.sp.jedit.jEdit;
@@ -12,26 +14,44 @@ import org.gjt.sp.jedit.jEdit;
 public class PropLogPlugin extends EBPlugin {
 
     public static String NAME = "KCMjEditPlugin";
-	public static String AUTHOR = "Benjamin Berman";
-	public static String VERSION = "0.0.1";
-	public static String OPTION_PREFIX = "options.kcmjeditplugin.";
-	private static PropLogPlugin INSTANCE;
+    public static String AUTHOR = "Benjamin Berman";
+    public static String VERSION = "0.0.1";
+    public static String OPTION_PREFIX = "options.kcmjeditplugin.";
+    private static PropLogPlugin INSTANCE;
 
-	@Override
-	public void start() {
-		INSTANCE = this;
-                
-                 Macros.message(jEdit.getActiveView(),
-                        "KCMjEditPlugin start() method called");
-	}
-	
-	@Override
-	public void stop() {
-               
-	}
-
-	public static PropLogPlugin getInstance() {
-		return INSTANCE;
-	}
+    JPanel kcmsHolder;
     
+    KCMS propLogKCMS;
+    
+    ThirtyKey mainRoot;
+    
+    @Override
+    public void start() {
+        INSTANCE = this;
+
+        Macros.message(jEdit.getActiveView(),
+                "PropLogPlugin start() method called");
+
+        
+        mainRoot = ThirtyKey.createRootCard();
+        propLogKCMS = new KCMS(mainRoot);
+        
+        
+        kcmsHolder = (JPanel) jEdit.getActiveView()
+                .getDockableWindowManager()
+                .getDockable("kcmsholder");
+        
+        kcmsHolder.add(propLogKCMS);
+        
+    }
+    
+    @Override
+    public void stop() {
+        kcmsHolder.remove(propLogKCMS);
+    }
+
+    public static PropLogPlugin getInstance() {
+        return INSTANCE;
+    }
+
 }
