fd7dcd63f486ccfd930c4a600c6bc6936528108b
Benjamin Berman <bnjmnbrmn@gmail.com>
2014-07-08 20:05:52 -0500

    Fixed dockable loading bug

--- src/edu/uiowa/cs/baberman/proplogjedit/PropLogPlugin.java
+++ src/edu/uiowa/cs/baberman/proplogjedit/PropLogPlugin.java
@@ -1,7 +1,6 @@
 package edu.uiowa.cs.baberman.proplogjedit;
 
 import edu.uiowa.cs.baberman.kcm.KCMS;
-import edu.uiowa.cs.baberman.kcm.KeyboardCard;
 import edu.uiowa.cs.baberman.kcm.SubmenuKey;
 import edu.uiowa.cs.baberman.kcm.ThirtyKey;
 import java.awt.event.KeyEvent;
@@ -10,10 +9,8 @@ import org.gjt.sp.jedit.Buffer;
 import org.gjt.sp.jedit.EBPlugin;
 import org.gjt.sp.jedit.Macros;
 import org.gjt.sp.jedit.View;
-import org.gjt.sp.jedit.buffer.JEditBuffer;
 import org.gjt.sp.jedit.gui.DockableWindowManager;
 import org.gjt.sp.jedit.jEdit;
-import org.gjt.sp.jedit.textarea.JEditTextArea;
 
 /**
  *
@@ -24,30 +21,43 @@ public class PropLogPlugin extends EBPlugin {
     public static String NAME = "KCMjEditPlugin";
     public static String AUTHOR = "Benjamin Berman";
     public static String VERSION = "0.0.1";
-    public static String OPTION_PREFIX = "options.kcmjeditplugin.";
+    public static String OPTION_PREFIX = "options.proplogplugin.";
     private static PropLogPlugin INSTANCE;
 
     JPanel kcmsHolder;
 
     KCMS propLogKCMS;
+    
+    public KCMS getPropLogKCMS() {
+        return propLogKCMS;
+    }
 
     ThirtyKey mainRoot;
 
     @Override
     public void start() {
+        
+        
         INSTANCE = this;
 
         View view = jEdit.getActiveView();
         DockableWindowManager dwm = view.getDockableWindowManager();
         
+        
+//        Macros.message(view, "start");
+        
         Buffer newFile;
         newFile = jEdit.newFile(view.getEditPane());
         
-        dwm.addDockableWindow("kcms");
-
-        propLogKCMS = (KCMS) dwm.getDockable("kcms");
+        mainRoot = ThirtyKey.createRootCard();
+        propLogKCMS = new KCMS(mainRoot);
+                
         
-        mainRoot = (ThirtyKey) propLogKCMS.getCurrentRoot();
+//        dwm.addDockableWindow("kcms");
+//
+//        propLogKCMS = (KCMS) dwm.getDockable("kcms");
+//        
+//        mainRoot = (ThirtyKey) propLogKCMS.getCurrentRoot();
 
         SubmenuKey<ThirtyKey> navKey;
         navKey = mainRoot.putNewSubmenu(KeyEvent.VK_F);
