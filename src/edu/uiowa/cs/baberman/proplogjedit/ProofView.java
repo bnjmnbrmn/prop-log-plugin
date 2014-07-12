1beab37572a245ba124e5388626989a6ecbaea88
Benjamin Berman <bnjmnbrmn@gmail.com>
2014-07-12 11:19:35 -0500

    Started work on keeping track of various TextAreaExtensions (which for simplicity will be immutable)

--- src/edu/uiowa/cs/baberman/proplogjedit/ProofView.java
+++ src/edu/uiowa/cs/baberman/proplogjedit/ProofView.java
@@ -1,8 +1,15 @@
 package edu.uiowa.cs.baberman.proplogjedit;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.gjt.sp.jedit.Buffer;
+import org.gjt.sp.jedit.EditPane;
+import org.gjt.sp.jedit.View;
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.textarea.TextArea;
+import org.gjt.sp.jedit.textarea.TextAreaExtension;
 
 /**
  *
@@ -11,16 +18,17 @@ import org.gjt.sp.jedit.Buffer;
 public class ProofView {
 
     final Buffer buffer;
-    
+
     Buffer getBuffer() {
         return buffer;
     }
 
-    final List<Highlight> highlights = new ArrayList<Highlight>();
-    
+    final Map<TextAreaExtension, TextArea> textAreaExtensionsWithTextAreas;
+
     private ProofModel proofModel;
 
     ProofView(Buffer buffer) {
+        this.textAreaExtensionsWithTextAreas = new HashMap<TextAreaExtension, TextArea>();
         this.buffer = buffer;
         buffer.setReadOnly(true);
     }
@@ -30,12 +38,30 @@ public class ProofView {
     }
 
     void update() {
-        
+
+        if (proofModel == null) {
+            throw new RuntimeException("Tried to update view with null model");
+        }
+
         buffer.setReadOnly(false);
         buffer.insert(0, proofModel.getRoot().getText());
         buffer.setReadOnly(true);
+
+        //remove all textareaextensions from their textareas and 
+        //from set of tracked textareaextensions
+        for (TextAreaExtension textAreaExtension
+                : textAreaExtensionsWithTextAreas.keySet()) {
+            textAreaExtensionsWithTextAreas
+                    .get(textAreaExtension)
+                    .getPainter()
+                    .removeExtension(textAreaExtension);
+        }
+        textAreaExtensionsWithTextAreas.clear();
         
-        //to do:  underline, potential spots, and highlights
+        //to do:  add appropriate TextAreaExtensions (Highlights, Underlines,
+        //and OptionalInsertionPointMarkers) to appropriate TextAreas' painters
+        //and to mapping of textAreaExtensions
         
+//       
     }
 }
---
+++ src/edu/uiowa/cs/baberman/proplogjedit/Underline.java
@@ -0,0 +1,12 @@
+
+package edu.uiowa.cs.baberman.proplogjedit;
+
+import org.gjt.sp.jedit.textarea.TextAreaExtension;
+
+/**
+ *
+ * @author bnjmnbrmn
+ */
+public class Underline extends TextAreaExtension {
+    
+}
