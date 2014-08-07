002a2a320c90c8d9f8ce373ccb457a49a2f34bca
Benjamin Berman <bnjmnbrmn@gmail.com>
2014-08-07 15:18:44 -0500

    Refactor PropVar's kcm tree initialization

--- src/edu/uiowa/cs/baberman/proplogjedit/nodes/PropVar.java
+++ src/edu/uiowa/cs/baberman/proplogjedit/nodes/PropVar.java
@@ -31,111 +31,15 @@ public final class PropVar extends Identifier {
                 getProofModel().appendStringToSelectedNode(letter);
             }
         }
-        String letter;
-        letter = "q";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.Q)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "w";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.W)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "e";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.E)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "r";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.R)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "t";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.T)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "y";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.Y)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "u";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.U)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "i";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.I)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "o";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.O)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "p";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.P)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "a";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.A)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "s";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.S)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "d";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.D)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "f";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.F)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "g";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.G)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "h";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.H)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "j";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.J)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "k";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.K)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "l";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.L)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "z";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.Z)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "x";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.X)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "c";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.C)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "v";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.V)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "b";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.B)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "n";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.N)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
-        letter = "m";
-        defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.M)
-                .setMenuItemText(letter)
-                .addPressAction(new LetterPressAction(letter));
+
+        for (ThirtyKey.KeyPosition kp : ThirtyKey.KeyPosition.values()) {
+            String letter = kp.getKeyLabel().toLowerCase();
+            if (Character.isLetter(letter.charAt(0))) {
+                defaultRootKeyboardCard.putNewLeaf(kp)
+                        .setMenuItemText(letter)
+                        .addPressAction(new LetterPressAction(letter));
+            }
+        }
 
         defaultRootKeyboardCard.putNewLeaf(ThirtyKey.KeyPosition.COMMA)
                 .setMenuItemText("Add Propositional Variable")
@@ -157,7 +61,7 @@ public final class PropVar extends Identifier {
                                     ProofModel.SelectionMode.LEAF);
 
                             getProofModel()
-                                .setSelectedNode(newPropVar);
+                            .setSelectedNode(newPropVar);
 
                             getProofModel().getProofView().update();
                         }
